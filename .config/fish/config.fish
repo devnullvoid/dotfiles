95c021fe3aa629dc6692d60f2f3623664b2c24fd
Jon Rogers <lonepie@gmail.com>
2025-06-23 02:03:38 -0400

    updates

---
+++ .config/fish/config.fish
@@ -0,0 +1,88 @@
+test ! -e "$HOME/.x-cmd.root/local/data/fish/rc.fish" || source "$HOME/.x-cmd.root/local/data/fish/rc.fish" # boot up x-cmd.
+# We are using -S to ensure the scope is correct
+# if test -z "$HOMEBREW_PREFIX"
+#   if test -e /home/linuxbrew/.linuxbrew/bin/brew
+#     eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+#   end
+# end
+
+if status is-interactive
+    starship init fish | source
+    enable_transience
+    #set -g BAT_THEME Dracula
+    # Commands to run in interactive sessions can go here
+    #set ayu_variant mirage
+    # set -a fish_complete_path ~/.local/share/fish/vendor_completions.d
+    # if test -n "$HOMEBREW_PREFIX"
+    # set -ag fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+    # scheme set catppuccin
+    # fish_config theme save "Catppuccin Frappe"
+    # fish_config theme save "Catppuccin Latte"
+    # fish_config theme save "Catppuccin Macchiato"
+    # fish_config theme save "Catppuccin Mocha"
+    pay-respects fish | source
+    tv init fish | source
+    set -x EXA_STANDARD_OPTIONS --long -aa --group --header --icons
+    alias ls="exa --icons -a"
+    # alias bat="bat --theme="
+    alias cat=bat
+    alias vim=nvim
+    set -x EDITOR nvim
+    # set -x FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
+    set -x fzf_preview_dir_cmd exa --all --color=always --icons
+    # set -x BAT_THEME Catppuccin-mocha
+    set -x SKIM_DEFAULT_OPTIONS $SKIM_DEFAULT_OPTIONS "--color=fg:#cdd6f4,bg:#1e1e2e,matched:#313244,matched_bg:#f2cdcd,current:#cdd6f4,current_bg:#45475a,current_match:#1e1e2e,current_match_bg:#f5e0dc,spinner:#a6e3a1,info:#cba6f7,prompt:#89b4fa,cursor:#f38ba8,selected:#eba0ac,header:#94e2d5,border:#6c7086"
+    # zoxide init fish | source
+    carapace _carapace | source
+    # carapace carapace fish | source
+    # thefuck --alias | source
+    zoxide init fish | source
+    atuin init fish | source
+    # end
+    if test "$TERM" = "xterm-kitty"
+        # override ssh to use kitty +kitten ssh
+        function ssh --description 'Use kitty +kitten ssh when in Kitty'
+            kitty +kitten ssh $argv
+        end
+    end
+    # test -f ~/.inshellisense/fish/init.fish && source ~/.inshellisense/fish/init.fish
+end
+
+# restic minio keys
+#set -x AWS_ACCESS_KEY_ID 1ZG05zGhvCas6Lk1
+#set -x AWS_SECRET_ACCESS_KEY clYakG9SFY4YAf4UyMN4i18Nq1yIZ51P
+
+#autorestic completion fish | source
+#op completion fish | source
+
+# function ssh
+#   set ps_res (ps -p (ps -p %self -o ppid= | xargs) -o comm=)
+#   # if [ "$ps_res" = "tmux" ]
+#   if set -q TMUX
+#     tmux rename-window (echo $argv | cut -d . -f 1)
+#     command ssh "$argv"
+#     tmux set-window-option automatic-rename "on" 1>/dev/null
+#   else
+#     command ssh "$argv"
+#   end
+# end
+
+
+# function _babelfish_source -S
+#   if test "$argv[1]" = '-' || string match -q '*.fish' "$argv[1]" || test -z "$argv[1]"
+#     builtin source $argv
+#   else
+#     babelfish < $argv[1] | builtin source
+#   end
+# end
+#
+# function source -S
+#   _babelfish_source $argv
+# end
+#
+# function . -S
+#   _babelfish_source $argv
+# end
+
+# source "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+
